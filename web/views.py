839f39e8cdb1b638d78a706f3da4a96848cd16a5
Baptiste Declerck <baptiste.declerck@reseau.eseo.fr>
2018-12-02 19:04:17 +0100

    import httpResponse

--- web/views.py
+++ web/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.template import loader
+from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
+    context = {}
     template = loader.get_template('web/html5up-story/index.html')
     return HttpResponse(template.render(context, request))
