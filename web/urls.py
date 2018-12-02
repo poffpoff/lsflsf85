56f3bc2e0a8863d6128798c73c617e6aea38aad7
Baptiste Declerck <baptiste.declerck@reseau.eseo.fr>
2018-12-02 19:26:43 +0100

    remove inde page

--- web/urls.py
+++ web/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index/', views.index, name='index'),
-    path('indexdemo/', views.indexdemo, name='indexdemo'),
+    path('', views.indexdemo, name='indexdemo'),
 ]
